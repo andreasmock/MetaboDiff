e05a0d6a58e5edcc2296131fb5d6a452c5d4e054
Andreas Mock <mock.science@gmail.com>
2017-07-04 10:43:34 +0200

    new functions

--- R/na_heatmap.R
+++ R/na_heatmap.R
@@ -7,10 +7,27 @@
 #' get_SMPDBanno(c("C00002", "C00020"))
 #' get_SMPDBanno(c("15422", "16027"))
 #'
-na_heatmap = function(met){
+na_heatmap = function(met, sample_label, label_colors) {
     met_na = is.na(assay(met))*1
     row_na = apply(met_na,1,sum)/ncol(met_na)
     col_na = apply(met_na,2,sum)/nrow(met_na)
+    if(missing(sample_label)&missing(label_colors)){
+        colanno = columnAnnotation(barplot=anno_barplot(col_na[order(col_na)],
+                                                        gp=gpar(fill="brown",
+                                                                col="brown"),
+                                                        axis=TRUE,
+                                                        border=FALSE))
+    } else {
+        col_list = list(grouping=label_colors)
+        names(col_list$grouping)=levels(sample_label)
+        colanno = columnAnnotation(df=data.frame(grouping=sample_label),
+                                   col=col_list,
+                                   barplot=anno_barplot(col_na[order(col_na)],
+                                                        gp=gpar(fill="brown",
+                                                                col="brown"),
+                                                        axis=TRUE,
+                                                        border=FALSE))
+    }
 
     rowanno = rowAnnotation(barplot=row_anno_barplot(row_na[order(row_na)],
                                                      gp=gpar(fill="brown",
@@ -18,11 +35,7 @@ na_heatmap = function(met){
                                                      axis = TRUE,
                                                      border=FALSE),
                             width=unit(1.5,"cm"))
-    colanno = columnAnnotation(barplot=anno_barplot(col_na[order(col_na)],
-                                                    gp=gpar(fill="brown",
-                                                            col="brown"),
-                                                    axis=TRUE,
-                                                    border=FALSE))
+
 
     ha = Heatmap(matrix = met_na[order(row_na),order(col_na)],
                  name="missing",
@@ -32,6 +45,7 @@ na_heatmap = function(met){
                  column_title = "samples",
                  column_title_side = "bottom",
                  show_column_names = FALSE,
+                 show_row_names = FALSE,
                  heatmap_legend_param = list(labels=c("no","yes")),
                  row_title= "metabolites",
                  bottom_annotation = colanno,
