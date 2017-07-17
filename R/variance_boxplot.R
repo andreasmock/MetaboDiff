f31978d3c1cf025d71bfc3f1ac3a0b2a7d5d2c38
Andreas Mock <mock.science@gmail.com>
2017-07-17 16:25:44 +0200

    add variance_boxplot.R function

--- R/variance_boxplot.R
+++ R/variance_boxplot.R
@@ -4,15 +4,14 @@
 #' @param rowAnnotation character name of rowAnnotation to stratify metabolites
 #' @return Boxplot comparing the distribution of variance in metabolic measurements across the pathways
 #' @examples
-#' variance_boxplot(met_example, rowAnnotation="Pathway.Name")
+#' variance_boxplot(met_example, rowAnnotation="SMPDB|Pathway.Name")
 variance_boxplot <- function(met, rowAnnotation) {
-    mdata = as.data.frame(longFormat(t(assays(met)[["norm_imputed"]]),
-                                     colDataCols=))
-    ggplot(mdata,
-           mapping=aes(x=colname,
+    df = data.frame(value=apply(assays(met)[["norm_imputed"]],1,var),
+                     pathway=as.vector(rowData(met[["norm_imputed"]])[[rowAnnotation]]))
+    df$pathway_ordered = reorder(df$pathway,df$value,median)
+    ggplot(df,
+           mapping=aes(x=pathway_ordered,
                        y=value,
-                       fill=rowData(met[["Pathway.Name"]]))) +
-        geom_boxplot() + xlab("") + ggtitle("raw") +
-        theme(axis.text.x=element_blank()) + ylab("log2(raw abundance)")
+                       fill=pathway_ordered)) + coord_flip() +
+        geom_boxplot() + xlab("") + guides(fill=FALSE) + ylab("vsn normalized abundance")
 }
-
